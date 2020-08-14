0142dba838abbb4a8a5b6f9d531b874314ee3943
LinMing <linmingku@163.com>
2020-08-15 01:06:42 +0800

    将新闻改成卡片式，并添加动画 将新闻做成瀑布流

--- src/pages/homePage/index.js
+++ src/pages/homePage/index.js
@@ -1,66 +1,73 @@
-import React, {useEffect , useState} from 'react';
-import { List, Card } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Card } from 'antd';
 import './index.less';
+import QueueAnim from 'rc-queue-anim';
 import $http from '@/api';
 
 // const { Meta } = Card
 
 function HomePage() {
-  // const arr = [1,2,3,4,5,6,7,8,9]
-  // const cardItem = arr.map(item => {
-  //       return <Card
-  //       cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-  //       bordered={false}>
-  //       <div>
-  //         <Meta title="Europe Street beat" description="www.instagram.com" />
-  //       </div>
-  //     </Card>
-  //   })
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
-  function getWangyi (keyword) {
-    setData([])
+  function getNews(keyword) {
+    setData([]);
     $http.getNews(keyword).then(res => {
-      let data = res.data.list
-      setData(data)
-    })
+      let data = res.data.list;
+      setData(data);
+    });
   }
 
   useEffect(() => {
-    getWangyi('news')
-  }, [])
+    getNews('news');
+  }, []);
 
-  let Cards = data.map(item => {
-    return <Card style={{ width: 300 }}>
+  function filteKey(num) {
+    return String.fromCharCode(num + 65);
+  }
+
+  let Cards = data.map((item, index) => {
+    return (
+      <Card hoverable key={filteKey(index)}>
         <div>
-          <img  style={{display:'block', width: '100%'}} alt={item.title} src={item.image} />
+          <img
+            style={{ display: 'block', width: '100%' }}
+            alt={item.title}
+            src={item.image}
+          />
         </div>
-        <p><a href={item.url}>{item.title}</a></p>
+        <h3>
+          <a href={item.url}>{item.title}</a>
+        </h3>
         <p>{item.brief}</p>
       </Card>
-  })
+    );
+  });
   return (
-    <div className="site-card-wrapper">
-      {/* <List
-          itemLayout="vertical"
-          dataSource={data}
-          renderItem={(item) => (
-            <List.Item
-              actions={[<div>{item.focus_date}</div>]}
-              extra={<img width={300} alt={item.title} src={item.image} />}
-            >
-              <List.Item.Meta
-                title={<a href={item.url}>{item.title}</a>}
-                description={item.brief}
-              />
-            </List.Item>
-          )}
-        /> */}
-        {
-          Cards
-        }
-    </div>
-  )
+    <>
+      {/* <div className={}>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+          <li>7</li>
+        </ul>
+      </div> */}
+      <div className="site-card-wrapper">
+        <QueueAnim
+          component="Card"
+          animConfig={[{ opacity: [1, 0], translateY: [0, 30] }, { height: 0 }]}
+          ease={['easeOutQuart', 'easeInOutQuart']}
+          duration={[550, 450]}
+          interval={150}
+        >
+          {Cards}
+        </QueueAnim>
+      </div>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
